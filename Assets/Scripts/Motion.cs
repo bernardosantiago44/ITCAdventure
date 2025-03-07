2968d730a2a1394ef1955485c4729e9954c9452c
Nacho <a01644423@tec.mx>
2025-03-07 14:21:37 -0600

    Dejé todo lo relacionado al personaje en Motion para no confundirnos

--- Assets/Scripts/Motion.cs
+++ Assets/Scripts/Motion.cs
@@ -11,12 +11,12 @@ public class CharacterController2D : MonoBehaviour
     private Rigidbody2D rb;
     private int maxJumps = 2;
     private int jumps = 0;
-    // private float groundCheckRadius = 0.2f;
+    // private float groundCheckRadius = 0.2f; // nse qué es esto
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 1.5f; // Ajusta la escala de gravedad según tus necesidades
+        rb.gravityScale = 1.5f; // Escala de gravedad
     }
 
     void Update()
@@ -31,7 +31,7 @@ public class CharacterController2D : MonoBehaviour
         Vector2 moveVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
         rb.linearVelocity = moveVelocity;
 
-        // Voltear el personaje según la dirección del movimiento
+        // Voltea el personaje según la dirección del movimiento
         if ((moveInput > 0 && transform.localScale.x < 0) || (moveInput < 0 && transform.localScale.x > 0))
         {
             Vector3 newScale = transform.localScale;
@@ -42,7 +42,6 @@ public class CharacterController2D : MonoBehaviour
 
     private void Jump()
     {
-
         if (jumps < maxJumps && Input.GetButtonDown("Jump"))
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
@@ -56,6 +55,20 @@ public class CharacterController2D : MonoBehaviour
         if (collision.gameObject.CompareTag("Piso"))
         {
             jumps = 0;
+        }
+
+        if (collision.gameObject.CompareTag("Boss"))
+        {
+            print("Oh no! Collision with " + collision.transform.name);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.CompareTag("WinZone"))
+        {
+            print("You Win!");
+            // SceneManager.LoadScene(nextSceneName); // Esto cambia la pantalla 
         }
     }
 
