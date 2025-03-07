92e0c7b91048a5955a15938f03c1e22bbda5b3a8
Nacho <a01644423@tec.mx>
2025-03-07 13:01:51 -0600

    agrega detección de colisión con un boss

--- Assets/Scripts/Termina el nivel.cs
+++ Assets/Scripts/Termina el nivel.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement; // Needed for scene loading
 public class WinTrigger : MonoBehaviour
 {
     public string winTag = "WinZone"; // Tag for the win object
+    public string bossTag = "Boss"; // Tag for the boss
     public string nextSceneName = "NextLevel"; // Change to your scene name
 
     void OnTriggerEnter2D(Collider2D other)
@@ -14,4 +15,14 @@ public class WinTrigger : MonoBehaviour
             // SceneManager.LoadScene(nextSceneName);
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(bossTag))
+        {
+            print("Oh no! Collision with " + collision.transform.name);
+        }
+    }
+
+
 }
