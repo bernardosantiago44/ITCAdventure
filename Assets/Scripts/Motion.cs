8781e2e7a3243d242bfb41602ab7275d978c51eb
Ali300805 <Ali.L.S@outlook.com>
2025-03-07 08:54:15 -0600

    el avance de las animaciones y asi

--- Assets/Scripts/Motion.cs
+++ Assets/Scripts/Motion.cs
@@ -1,59 +1,52 @@
 using UnityEngine;
 
-public class Motion : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
-    // Editor available variables
-    [SerializeField] private float _speed = 5.0f;
-    [SerializeField] private bool _dynamicMotion = false;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float groundCheckRadius = 0.2f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 1.5f; // Ajusta la escala de gravedad según tus necesidades
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Polling for input
-        // poll directly to a device
-        // true if key was up last frame but down this frame
-        // if (Input.GetKeyDown(KeyCode.O)) {
-        //     print("Key Down O");
-        // }
+        Move();
+        Jump();
+    }
 
-        // if (Input.GetKey(KeyCode.O)) {
-        //     print("Key O");
-        // }
+    private void Move()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector2 moveVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = moveVelocity;
 
-        // if (Input.GetKeyUp(KeyCode.O)) {
-        //     print("Key Up O");
-        // }
-
-        // // Also poll the mouse
-        // if (Input.GetMouseButtonDown(0)) {
-        //     print("Mouse Down 0");
-        // }
-
-        // Axes are a way to abstract input from different devices
-        // GetAxis returns a float between -1 and 1
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        // The closer the object is from the center,
-        // the slower it moves
-        float speed = this._speed;
-        if (this._dynamicMotion) {
-            float distance = Vector3.Distance(Vector3.zero, transform.position);
-            speed = this._speed * Mathf.Max(distance / 10, 0.01f); // Limit the speed to 1% of the original speed
+        // Voltear el personaje según la dirección del movimiento
+        if (moveInput > 0 && transform.localScale.x < 0 || moveInput < 0 && transform.localScale.x > 0)
+        {
+            Vector3 newScale = transform.localScale;
+            newScale.x *= -1;
+            transform.localScale = newScale;
         }
+    }
 
-        transform.Translate(
-            horizontal * Time.deltaTime * speed,
-            vertical * Time.deltaTime * speed,
-            0
-        );
+    private void Jump()
+    {
+        // Verificar si el personaje está tocando el suelo
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 }
