492f2afffa7d4488d3f45cc335b6a917c0d5d0d7
Nacho <a01644423@tec.mx>
2025-04-04 08:02:44 -0600

    solved bullet collitions (even added small feat) and floor hitbox

--- Assets/Scripts/chatgpt_script.cs
+++ Assets/Scripts/chatgpt_script.cs
@@ -26,6 +26,9 @@ public class chatgpt_script : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.CompareTag("Boss"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
